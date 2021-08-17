3228b8ebba6d70174bdb674b6d286cb34a444873
Roshan <rshnrasal@gmail.com>
2021-08-17 17:15:02 +0530

    Calculator-files

---
+++ index.js
@@ -0,0 +1,31 @@
+function addSum() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    let sum = parseInt(a) + parseInt(b);
+    let output1 = document.getElementById("output");
+    output1.innerHTML = "<b>Addition is:  " + sum + "</b>";
+}
+
+function multiply() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    let sum = parseFloat(a) * parseFloat(b);
+    let output1 = document.getElementById("output");
+    output1.innerHTML = "<b>Multiplication is:  " + sum + "</b>";
+}
+
+function division() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    let div = parseFloat(a) / parseFloat(b);
+    let output1 = document.getElementById("output");
+    output1.innerHTML = "<b>Division is:  " + div + "</b>";
+}
+
+function subtraction() {
+    let a = document.getElementById("num1").value;
+    let b = document.getElementById("num2").value;
+    let sub = parseFloat(a) - parseFloat(b);
+    let output1 = document.getElementById("output");
+    output1.innerHTML = "<b>Subtraction is:  " + sub + "</b>";
+}
